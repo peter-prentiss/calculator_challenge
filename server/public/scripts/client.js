3e0476a873a205f5946e39a9730f16d333043f6e
Peter Prentiss <peter.prentiss@gmail.com>
2017-06-30 17:21:52 -0500

    complete base mode and set up numpad

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,45 @@
 console.log('js sourced ');
-
+let button = "add";
 
 $(document).ready(function() {
 
-  
+  $('.operator').click(function() {
+    button = $(this).attr('id');
+    console.log(button);
+  })
+  $('#equals').click(buildEquation);
 
 });
+
+function buildEquation() {
+  let firstValue = $('#val1').val();
+  let secondValue = $('#val2').val();
+  let operator = button;
+  console.log(firstValue);
+  console.log(secondValue);
+  console.log(button);
+  $.ajax({
+    type: 'POST',
+    url: '/calculate',
+    data: {
+      firstValue: firstValue,
+      secondValue: secondValue,
+      operator: operator
+    },
+    success: function(response) {
+      console.log('responded');
+      console.log(response);
+      answer();
+    }
+  })
+}
+
+function answer() {
+  $.ajax({
+    type: 'GET',
+    url: '/calculate',
+    success: function(response) {
+      $('#answer').text(response);
+    }
+  })
+}
--- server/server.js
+++ server/server.js
@@ -4,6 +4,32 @@ let path = require('path');
 let port = 6969;
 let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+let firstValue;
+let secondValue;
+let operator;
+let answer;
+
+app.post('/calculate', function(req, res) {
+  let equation = req.body;
+  firstValue = parseInt(equation.firstValue);
+  secondValue = parseInt(equation.secondValue);
+  operator = equation.operator;
+  if (operator === "add") {
+    answer = firstValue + secondValue;
+  } else if (operator === "subtract") {
+    answer = firstValue - secondValue;
+  } else if (operator === "multiply") {
+    answer = firstValue * secondValue;
+  } else if (operator === "divide") {
+    answer = firstValue / secondValue;
+  }
+  console.log(answer);
+  res.sendStatus(200);
+});
+
+app.get('/calculate', function(req,res) {
+  res.send(answer.toString());
+})
 
 app.get('/*', function(req, res) {
   let file = req.params[0] || '/views/index.html';
